1caa2a1d5ed92313aa04089122a87fde4c8ca3a2
BogdanMaria <>
2023-10-01 10:05:40 +0000

    Add more questions to the question array

--- assets/javascript/play.js
+++ assets/javascript/play.js
@@ -88,4 +88,84 @@ let questions = [
     choice4: "NaCl",
     answer: 1
     },
+    {
+        question: "Which gas makes up the majority of Earth's atmosphere?",
+        choice1: "Oxygen",
+        choice2: "Nitrogen",
+        choice3: "Carbon Dioxide",
+        choice4: "Argon",
+        answer: 2
+    },
+    {
+        question: "What is the largest organ in the human body?",
+        choice1: "Heart",
+        choice2: "Brain",
+        choice3: "Skin",
+        choice4: "Liver",
+        answer: 3
+    },
+    {
+        question: "Who painted the Mona Lisa?",
+        choice1: "Pablo Picasso",
+        choice2: "Leonardo da Vinci",
+        choice3: "Vincent van Gogh",
+        choice4: "Michelangelo",
+        answer: 2
+    },
+    {
+        question: "What is the smallest prime number?",
+        choice1: "0",
+        choice2: "1",
+        choice3: "2",
+        choice4: "3",
+        answer: 3
+    },
+    {
+        question: "Which of the following is a programming paradigm?",
+        choice1: "Pop music",
+        choice2: "Object-Oriented Programming",
+        choice3: "Astronomy",
+        choice4: "Cooking",
+        answer: 2
+    },
+    {
+        question: "Which planet is known as the 'Morning Star' or 'Evening Star'?",
+        choice1: "Venus",
+        choice2: "Mars",
+        choice3: "Jupiter",
+        choice4: "Saturn",
+        answer: 1
+    },
+    {
+        question: "What is the chemical symbol for oxygen?",
+        choice1: "O2",
+        choice2: "H2O",
+        choice3: "O3",
+        choice4: "CO2",
+        answer: 1
+    },
+    {
+        question: "In computer science, what does 'HTML' stand for?",
+        choice1: "HyperText Markup Language",
+        choice2: "High-Level Technical Markup Language",
+        choice3: "Human-Tested Markup Language",
+        choice4: "Hyperlink and Text Markup Language",
+        answer: 1
+    },
+    {
+        question: "Which country is known as the Land of the Rising Sun?",
+        choice1: "China",
+        choice2: "South Korea",
+        choice3: "Japan",
+        choice4: "Thailand",
+        answer: 3
+    },
+    {
+        question: "What is the largest desert in the world?",
+        choice1: "Sahara Desert",
+        choice2: "Gobi Desert",
+        choice3: "Arctic Desert",
+        choice4: "Atacama Desert",
+        answer: 1
+    }
 ]
