e6a6bbec122ef2988dea020b0e4f9d2cf5362bd6
BogdanMaria <>
2023-10-05 12:33:08 +0000

    Create the High Scores array

--- assets/javascript/end.js
+++ assets/javascript/end.js
@@ -2,11 +2,23 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+console.log(highScores)
+
 finalScore.innerText = mostRecentScore;
+
 username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
 });
 
-function saveHighScore(e) {
+saveHighScore = e => {
     console.log("clicked the save button");
-}
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    console.log(highScores);
+};
