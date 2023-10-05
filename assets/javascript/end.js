7d7a79ecc987a980d5a4e0dad724249bb817601b
BogdanMaria <>
2023-10-05 12:42:20 +0000

    Set the max number of high scores to 5 and sort it

--- assets/javascript/end.js
+++ assets/javascript/end.js
@@ -3,6 +3,7 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const maxHighScores = 5;
 console.log(highScores)
 
 finalScore.innerText = mostRecentScore;
@@ -20,5 +21,7 @@ saveHighScore = e => {
         name: username.value,
     };
     highScores.push(score);
+    highScores.sort ( (a, b) => b.score - a.score);
+    highScores.splice(5);
     console.log(highScores);
 };
