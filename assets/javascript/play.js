9f8736da9d08fca1c39fbb8672bdcc448b3ef5f4
BogdanMaria <>
2023-10-05 08:35:38 +0000

    Create new question function and display random question

--- assets/javascript/play.js
+++ assets/javascript/play.js
@@ -9,84 +9,84 @@ let availableQuestions = [];
 
 let questions = [
     {
-    question: "What is the capital of France?",
-    choice1: "Madrid",
-    choice2: "Berlin",
-    choice3: "Paris",
-    choice4: "Rome",
-    answer: 3
+        question: "What is the capital of France?",
+        choice1: "Madrid",
+        choice2: "Berlin",
+        choice3: "Paris",
+        choice4: "Rome",
+        answer: 3
     },
     {
-    question: "Which planet is known as the 'Red Planet'?",
-    choice1: "Mars",
-    choice2: "Venus",
-    choice3: "Jupiter",
-    choice4: "Saturn",
-    answer: 1
+        question: "Which planet is known as the 'Red Planet'?",
+        choice1: "Mars",
+        choice2: "Venus",
+        choice3: "Jupiter",
+        choice4: "Saturn",
+        answer: 1
     },
     {
-    question: "What is the largest mammal in Earth?",
-    choice1: "Elephant",
-    choice2: "Blue Whale",
-    choice3: "Giraffe",
-    choice4: "Hippopotamus",
-    answer: 2
+        question: "What is the largest mammal in Earth?",
+        choice1: "Elephant",
+        choice2: "Blue Whale",
+        choice3: "Giraffe",
+        choice4: "Hippopotamus",
+        answer: 2
     },
     {
-    question: "Which gas do plants absorb from the atmosphere?",
-    choice1: "Carbon Monoxide",
-    choice2: "Nitrogen",
-    choice3: "Oxygen",
-    choice4: "Carbon Dioxide",
-    answer:4
+        question: "Which gas do plants absorb from the atmosphere?",
+        choice1: "Carbon Monoxide",
+        choice2: "Nitrogen",
+        choice3: "Oxygen",
+        choice4: "Carbon Dioxide",
+        answer: 4
     },
     {
-    question: "What is the chemical symbol for gold?",
-    choice1: "Ag",
-    choice2: "Au",
-    choice3: "Fe",
-    choice4: "Cu",
-    answer: 2
+        question: "What is the chemical symbol for gold?",
+        choice1: "Ag",
+        choice2: "Au",
+        choice3: "Fe",
+        choice4: "Cu",
+        answer: 2
     },
     {
-    question: "which famous scientis formulated the theory of relativity",
-    choice1: "Isaac Newton",
-    choice2: "Galileo Galiler",
-    choice3: "Stephen Hawking",
-    choice4: "Albert Einstein",
-    answer: 4
+        question: "which famous scientis formulated the theory of relativity",
+        choice1: "Isaac Newton",
+        choice2: "Galileo Galiler",
+        choice3: "Stephen Hawking",
+        choice4: "Albert Einstein",
+        answer: 4
     },
     {
-    question: "Which programming language is often used for web develpment?",
-    choice1: "Python",
-    choice2: "Java",
-    choice3: "JavaScript",
-    choice4: "C++",
-    answer: 3
+        question: "Which programming language is often used for web develpment?",
+        choice1: "Python",
+        choice2: "Java",
+        choice3: "JavaScript",
+        choice4: "C++",
+        answer: 3
     },
     {
-    question: "What is the largest planet in our solar System?",
-    choice1: "Earth",
-    choice2: "Mars",
-    choice3: "Jupiter",
-    choice4: "Saturn",
-    answer: 3
+        question: "What is the largest planet in our solar System?",
+        choice1: "Earth",
+        choice2: "Mars",
+        choice3: "Jupiter",
+        choice4: "Saturn",
+        answer: 3
     },
     {
-    question: "Which famous playwright wrote 'Romeo and Juliet'",
-    choice1: "Charles Dickens",
-    choice2: "William Shakespeare",
-    choice3: "Jane Austen",
-    choice4: "Leo Tolstoy",
-    answer: 2
+        question: "Which famous playwright wrote 'Romeo and Juliet'",
+        choice1: "Charles Dickens",
+        choice2: "William Shakespeare",
+        choice3: "Jane Austen",
+        choice4: "Leo Tolstoy",
+        answer: 2
     },
     {
-    question: "What is the chemical symbol for water?",
-    choice1: "H2O",
-    choice2: "CO2",
-    choice3: "O2",
-    choice4: "NaCl",
-    answer: 1
+        question: "What is the chemical symbol for water?",
+        choice1: "H2O",
+        choice2: "CO2",
+        choice3: "O2",
+        choice4: "NaCl",
+        answer: 1
     },
     {
         question: "Which gas makes up the majority of Earth's atmosphere?",
@@ -168,18 +168,26 @@ let questions = [
         choice4: "Atacama Desert",
         answer: 1
     }
-]
+];
 
 //Constants
 
 const correctBonus = 10;
 const maxQuestions = 5;
 
-startGame = () => {
+function startGame() {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions);
+    getNewQuestion();
 };
+
+function getNewQuestion() {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+}
 
 startGame();
