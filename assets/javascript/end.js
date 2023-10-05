c7b495762c5848fcd02de518d637434fa716e1a1
BogdanMaria <>
2023-10-05 11:56:47 +0000

    Disable the save button until there is text input

--- assets/javascript/end.js
+++ assets/javascript/end.js
@@ -1,3 +1,10 @@
-function saveHighScore(event) {
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+
+username.addEventListener("keyup", () => {
+    saveScoreBtn.disabled = !username.value;
+})
+
+function saveHighScore(e) {
     console.log("clicked the save button");
 }
