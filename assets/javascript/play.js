207d66216373ebd0c1dfb870a602a852783ac4b7
BogdanMaria <>
2023-10-08 09:04:14 +0000

    Add title and mockup to ReadMe

--- assets/javascript/play.js
+++ assets/javascript/play.js
@@ -187,7 +187,7 @@ function startGame() {
 function getNewQuestion() {
     if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
         localStorage.setItem('mostRecentScore', score);
-        return window.location.assign("/end.html");
+        return window.location.assign("end.html");
     }
     questionCounter++;
     questionCounterText.innerText = `${questionCounter}/${maxQuestions}`;
